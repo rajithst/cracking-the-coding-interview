f46bf63414d59f37655b4491f0ab45a22f00f6bb
rajithst <rajiththennakoonshady@gmail.com>
2022-03-29 21:21:54 +0900

    Graphs - add comments

--- Trees and Graphs/1.route_between_nodes.py
+++ Trees and Graphs/1.route_between_nodes.py
@@ -3,7 +3,10 @@ from typing import List
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # map to keep track of visited nodes
         visited = {i: False for i in range(n)}
+
+        # since nodes are bidirectional, create bidirectional adjacency list
         adj_list = {}
         for i in range(len(edges)):
             v1, v2 = edges[i]
@@ -15,10 +18,15 @@ class Solution:
             adj_list[v1].append(v2)
             adj_list[v2].append(v1)
 
+        # use dfs to find if there is a path from source to destination
+        # time complexity: O(V + E)
+        # space complexity: O(V)
         def dfs(node, d):
             visited[node] = True
+            # if destination is found, return True
             if node == d:
                 return True
+            # otherwise iterate through all adjacent nodes
             for ch in adj_list[node]:
                 if not visited[ch]:
                     v = dfs(ch, d)
